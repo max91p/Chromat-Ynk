611bde2ab9f3aa2eebcc9e76a6d49e2961b37ab6
tomlachenaud <tomlachenaud@gmail.com>
2024-05-17 22:01:29 +0200

    test de récupération de text

--- demo/src/Main.java
+++ demo/src/Main.java
@@ -1,10 +1,8 @@
 package src;
 
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -15,32 +13,13 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage){
 
-        ScrollPane scrollPane = new ScrollPane();
+        MainView view = new MainView(10);
+        ScrollPane scrollPane = new ScrollPane(view);
         scrollPane.setFitToWidth(true);
 
-        Scene mainViewScene = new Scene(scrollPane, WINDOW_WIDTH, WINDOW_HEIGHT);
-
-
-        Group root = new Group();
-
-        // Ajout de quelques éléments au groupe (par exemple, des cercles)
-        Circle circle1 = new Circle(50, 50, 30);
-        Circle circle2 = new Circle(150, 50, 30);
-        Circle circle3 = new Circle(250, 50, 30);
-
-        root.getChildren().addAll(circle1, circle2, circle3);
-
         // Création de la scène avec la racine
-        Scene scene = new Scene(root, 400, 200);
+        Scene scene = new Scene(scrollPane, 400, 200);
 
-        // Récupération de la racine de la scène
-        Group sceneRoot = (Group) scene.getRoot();
-
-        // Suppression du dernier élément ajouté à la racine de la scène
-        int lastIndex = sceneRoot.getChildren().size() - 1;
-        if (lastIndex >= 0) {
-            sceneRoot.getChildren().remove(lastIndex);
-        }
 
         primaryStage.setScene(scene);
         primaryStage.show();
---
+++ demo/src/MainView.java
@@ -0,0 +1,36 @@
+package src;
+
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class MainView extends VBox {
+    public void getText(TextArea text, Text test){
+        test.setText(text.getText());
+
+
+    }
+    public MainView(double spacing){
+        super(spacing);
+        ObservableList<Node> components = this.getChildren();
+
+        TextArea text = new TextArea();
+        Button button = new Button("Submit");
+        Text test = new Text();
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // Appel de la méthode que vous souhaitez exécuter
+                getText(text,test);
+            }
+        });
+        components.addAll(text,button,test);
+    }
+
+
+}
