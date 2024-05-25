1f37714e010fe72e457a0ca1db2552fe09b1eb9a
assia <assia.brouzi@gmail.com>
2024-05-25 18:17:59 +0200

    modif exceptions

--- demo/src/main/java/com/example/ColorOfLine.java
+++ demo/src/main/java/com/example/ColorOfLine.java
@@ -17,12 +17,16 @@ public class ColorOfLine {
      * @param blue the blue component of the new color
      */
     public ColorOfLine(int red, int green, int blue){
-        if(red < 0 || red > 255 || green < 0 || green > 255 || blue < 0 || blue > 255){
-            throw new IllegalArgumentException("Les valeurs de rouge, vert et bleu doivent être comprises entre 0 et 255");
-        } else {
-            this.red = red;
-            this.green = green;
-            this.blue = blue;
+        try {
+            if (red < 0 || red > 255 || green < 0 || green > 255 || blue < 0 || blue > 255) {
+                throw new ErrorLogger("Les valeurs de rouge, vert et bleu doivent être comprises entre 0 et 255");
+            } else {
+                this.red = red;
+                this.green = green;
+                this.blue = blue;
+            }
+        }catch (ErrorLogger e){
+            e.logError();
         }
     }
         
@@ -33,13 +37,17 @@ public class ColorOfLine {
      * @param hex the hexadecimal code of the new color
      */
     public ColorOfLine(String hex){
-        if(hex.length() != 7 || hex.charAt(0) != '#' || !hex.substring(1).matches("[0-9A-F]+")){
-            throw new IllegalArgumentException("Le code hexadécimal doit être sous la forme #RRGGBB où RR, GG et BB sont des valeurs hexadécimales comprises entre 00 et FF");
-        }
-        else{
-            this.red = Integer.parseInt(hex.substring(1, 3), 16);
-            this.green = Integer.parseInt(hex.substring(3, 5), 16);
-            this.blue = Integer.parseInt(hex.substring(5, 7), 16);
+        try{
+            if(hex.length() != 7 || hex.charAt(0) != '#' || !hex.substring(1).matches("[0-9A-F]+")){
+                throw new ErrorLogger("Le code hexadécimal doit être sous la forme #RRGGBB où RR, GG et BB sont des valeurs hexadécimales comprises entre 00 et FF");
+            }
+            else{
+                this.red = Integer.parseInt(hex.substring(1, 3), 16);
+                this.green = Integer.parseInt(hex.substring(3, 5), 16);
+                this.blue = Integer.parseInt(hex.substring(5, 7), 16);
+            }
+        }catch (ErrorLogger e){
+            e.logError();
         }
     }
         
@@ -52,12 +60,16 @@ public class ColorOfLine {
     * @param blue the blue component of the new color
     */
     public ColorOfLine(double red, double green, double blue){
-        if(red < 0 || red > 1 || green < 0 || green > 1 || blue < 0 || blue > 1){
-            throw new IllegalArgumentException("Les valeurs de rouge, vert et bleu doivent être comprises entre 0 et 1");
-        } else {
-            this.red = (int) (red * 255);
-            this.green = (int) (green * 255);
-            this.blue = (int) (blue * 255);
+        try{
+            if(red < 0 || red > 1 || green < 0 || green > 1 || blue < 0 || blue > 1){
+                throw new ErrorLogger("Les valeurs de rouge, vert et bleu doivent être comprises entre 0 et 1");
+            } else {
+                this.red = (int) (red * 255);
+                this.green = (int) (green * 255);
+                this.blue = (int) (blue * 255);
+            }
+        }catch (ErrorLogger e){
+            e.logError();
         }
     }
 
@@ -94,10 +106,14 @@ public class ColorOfLine {
      * @param red the new red component of this color
      */
     public void setRed(int red){
-        if(red < 0 || red > 255){
-            throw new IllegalArgumentException("La valeur de rouge doit être comprise entre 0 et 255");
-        } else {
-            this.red = red;
+        try{
+            if(red < 0 || red > 255){
+                throw new ErrorLogger("La valeur de rouge doit être comprise entre 0 et 255");
+            } else {
+                this.red = red;
+            }
+        }catch (ErrorLogger e){
+            e.logError();
         }
     }
 
@@ -107,10 +123,14 @@ public class ColorOfLine {
      * @param green the new green component of this color
      */
     public void setGreen(int green){
-        if(green < 0 || green > 255){
-            throw new IllegalArgumentException("La valeur de vert doit être comprise entre 0 et 255");
-        } else {
-            this.green = green;
+        try{
+            if(green < 0 || green > 255){
+                throw new ErrorLogger("La valeur de vert doit être comprise entre 0 et 255");
+            } else {
+                this.green = green;
+            }
+        }catch (ErrorLogger e){
+            e.logError();
         }
     }
 
@@ -120,10 +140,14 @@ public class ColorOfLine {
      * @param blue the new blue component of this color
      */
     public void setBlue(int blue){
-        if(blue < 0 || blue > 255){
-            throw new IllegalArgumentException("La valeur de bleu doit être comprise entre 0 et 255");
-        } else {
-            this.blue = blue;
+        try{
+            if(blue < 0 || blue > 255){
+                throw new ErrorLogger("La valeur de bleu doit être comprise entre 0 et 255");
+            } else {
+                this.blue = blue;
+            }
+        }catch (ErrorLogger e){
+            e.logError();
         }
     }
 
